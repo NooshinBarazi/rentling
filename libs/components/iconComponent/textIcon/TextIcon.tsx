0ad4f23ae8ceab28492c2645aab36f581f90417c
NaderBhrr <nader.bhr.devv@gmail.com>
2023-01-29 19:49:25 +0330

    refacotr: change the props to TextIcon component

--- libs/components/iconComponent/textIcon/TextIcon.tsx
+++ libs/components/iconComponent/textIcon/TextIcon.tsx
@@ -1,24 +1,27 @@
 import React from 'react';
 import style from './texticon.module.scss';
 
-type Icons = 'call' | 'instagram'; 
+type Icon = 'call' | 'instagram'; 
 
-const getIconFromName = (iconName: Icons) => {
+const getIconFromName = (iconName: Icon) => {
   switch (iconName) {
     case 'call':
       return;
   }
 };
 
-const TextIcon = ({children,iconName}: {children: any; iconName: Icons;}) => {
-const icon = getIconFromName(iconName);
+interface IProps {
+  Icon: React.ReactNode;
+  text: string;
+}
+
+export const TextIcon = ({Icon, text}: IProps) => {
+// const icon = getIconFromName(iconName);
 
   return (
     <div className={style.textIcon}>
-      {icon}
-      {children}
+      {Icon}
+      {text}
     </div>
   );
 };
-
-export default TextIcon;
