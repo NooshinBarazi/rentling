29ecc0d38180b6a9077b5817bf30aa53388bc312
nooshinbarazi <nooshinbry@gmail.com>
2023-02-04 01:19:47 +0330

    feat: add selectInput component

---
+++ libs/fr-shared/src/components/elements/SelectInput/SelectInput.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { FieldValues, UseFormRegister } from 'react-hook-form';
+
+interface IOption {
+  value: string;
+  label: string;
+}
+
+interface IProps {
+  name: string;
+  register: UseFormRegister<FieldValues>;
+  options: IOption[];
+}
+
+export const SelectInput = ({ name, register, options }: IProps) => {
+  return (
+    <select {...register(name)}>
+      {options.map((option) => (
+        <option key={option.value} value={option.value}>
+          {option.label}
+        </option>
+      ))}
+    </select>
+  );
+};
