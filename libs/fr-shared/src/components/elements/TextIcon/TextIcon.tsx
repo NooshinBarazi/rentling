47fb764d63a3122aef79f72aa88f9a04274848d4
nooshinbarazi <nooshinbry@gmail.com>
2023-02-15 23:00:27 +0330

    fix: change textIcon

--- libs/fr-shared/src/components/elements/TextIcon/TextIcon.tsx
+++ libs/fr-shared/src/components/elements/TextIcon/TextIcon.tsx
@@ -1,27 +1,17 @@
 import React from 'react';
 import style from './texticon.module.scss';
 
-type Icon = 'call' | 'instagram';
-
-const getIconFromName = (iconName: Icon) => {
-  switch (iconName) {
-    case 'call':
-      return;
-  }
-};
-
 interface IProps {
   Icon: React.ReactNode;
   text: string;
+  newStyle: string;
 }
 
-export const TextIcon = ({ Icon, text }: IProps) => {
-  // const icon = getIconFromName(iconName);
-
+export const TextIcon = ({ Icon, text, newStyle }: IProps) => {
   return (
-    <div className={style.textIcon}>
-      {Icon}
-      {text}
+    <div className={newStyle || style.text_icon}>
+      <span>{Icon}</span>
+      <p>{text}</p>
     </div>
   );
 };
