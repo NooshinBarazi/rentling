340a6cd70c976d74b170763d4e9bfd58ca29a185
nooshinbarazi <nooshinbry@gmail.com>
2023-02-19 21:05:03 +0330

    fix: fix problem

--- libs/fr-shared/src/components/elements/Form/Form.tsx
+++ libs/fr-shared/src/components/elements/Form/Form.tsx
@@ -1,14 +1,15 @@
-import { useForm } from "react-hook-form";
+import { useForm } from 'react-hook-form';
 
 interface IFormProps{
     children: any;
+    style: string;
     onSubmit: (value: any) => void;
 }
 
- export const Form = ({children, onSubmit}: IFormProps) => {
+ export const Form = ({children,style, onSubmit}: IFormProps) => {
     const {handleSubmit, formState: {errors}} = useForm()
     return ( 
-        <form onSubmit={handleSubmit(onSubmit)}>
+        <form onSubmit={handleSubmit(onSubmit)} className={style}>
             {children}
             {errors}
         </form>
