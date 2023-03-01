31a820404397eca761955a7e976d359b2486ba20
Mehran Barmouz <mehranbarmouz@gmail.com>
2023-03-01 15:23:10 +0330

    fix(conflict-fix): fix files with conflict problem

--- libs/fr-shared/src/components/elements/TextIcon/TextIcon.tsx
+++ libs/fr-shared/src/components/elements/TextIcon/TextIcon.tsx
@@ -1,32 +1,6 @@
 import React from 'react';
 import style from './texticon.module.scss';
 
-<<<<<<< HEAD
-type Icon = 'call' | 'instagram';
-
-const getIconFromName = (iconName: Icon) => {
-  switch (iconName) {
-    case 'call':
-      return;
-  }
-};
-
-interface IProps {
-  Icon: React.ReactNode;
-  text: string;
-}
-
-export const TextIcon = ({ Icon, text }: IProps) => {
-  // const icon = getIconFromName(iconName);
-
-  return (
-    <div className={style.textIcon}>
-      {Icon}
-      {text}
-    </div>
-  );
-};
-=======
 interface IProps {
   Icon: React.ReactNode;
   text: string;
@@ -41,4 +15,3 @@ export const TextIcon = ({ Icon, text, newStyle }: IProps) => {
     </div>
   );
 };
->>>>>>> c869776faf3e3d92bdeb2d83e268a6409be0281c
