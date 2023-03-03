5699cbce000f3ec0955129a889f8a7dea146379c
ranaweysi <ranaweysi@gmail.com>
2023-03-03 23:22:56 +0330

    fix: Completion of the component houseDetails

--- libs/fr-shared/src/components/HousePayment/houseDetails.tsx
+++ libs/fr-shared/src/components/HousePayment/houseDetails.tsx
@@ -1,37 +1,58 @@
+import { useForm } from 'react-hook-form';
 import { Button } from '@rentling/fr-shared';
 import { Input } from '@rentling/fr-shared';
-import style from './houseDetails.module.scss';
+import styles from './houseDetails.module.scss';
+import { CalenderIcon } from '@rentling/fr-shared';
 
 export const HouseDetails = () => {
+  const { register } = useForm();
   return (
-    <section id={style.section_houseDetails}>
-      <div className={style.main_container}>
-        <div className={style.container_text}>
-          <p>REnt this house</p>
-          <div className={style.button_container}>
-            <Button
+    <section id={styles.section_houseDetails}>
+      <div className={styles.main_container}>
+        <div className={styles.container_text}>
+          <p>Rent this house</p>
+        </div>
+        <div className={styles.button_style}>
+          <Button
+            Icon={undefined}
+            text="Daily"
+            onClick={() => {}}
+            newStyle={styles.actived_button}
+          />
+          <Button
+            Icon={undefined}
+            text="Daily"
+            newStyle={styles.deactive_button}
+            onClick={() => {}}
+          />
+        </div>
+        <div className={styles.text_price}>
+          <p>4,100</p>
+          <span>$/mo</span>
+        </div>
+        <form className={styles.form_container}>
+          <div className={styles.input_container}>
+            <Input
               Icon={undefined}
-              text="Daily"
-              onClick={() => {}}
-              newStyle=""
-            />
-            <Button
-              Icon={undefined}
-              text="Daily"
-              newStyle={style.button_style}
-              onClick={() => {}}
+              type="Date"
+              name="startDate"
+              register={register}
+              required={true}
+              id="undefined"
             />
           </div>
-          <div className={style.text_price}>
-            <p>4,100</p>
-            <span>$/mo</span>
+          <div className={styles.input_container}>
+            <Input
+              Icon={undefined}
+              type="Date"
+              name="EndtDate"
+              register={register}
+              required={true}
+              id="undefined"
+            />
           </div>
-        </div>
-        <div className={style.input_container}>
-          <input type="Date" placeholder="Starts date" />
-          <input type="Date" placeholder="Ends date" />
-        </div>
-        <div>
+        </form>
+        <div className={styles.button_style}>
           <Button
             Icon={undefined}
             text="RESERVE"
