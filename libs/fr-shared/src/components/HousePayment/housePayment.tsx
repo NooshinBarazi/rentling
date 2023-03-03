4ddfc2b8027cd83ab4e4092a679295f6f5e0e0c4
ranaweysi <ranaweysi@gmail.com>
2023-03-03 23:25:31 +0330

    fix: Completion of the housePayment component

--- libs/fr-shared/src/components/HousePayment/housePayment.tsx
+++ libs/fr-shared/src/components/HousePayment/housePayment.tsx
@@ -2,22 +2,29 @@ import Image from 'next/image';
 import house2 from './image/house2.jpg';
 import house3 from './image/house3.jpg';
 import house4 from './image/house4.jpg';
-import Style from './housePayment.module.scss';
+import Styles from './housePayment.module.scss';
+import { SlidebarButton } from '@rentling/fr-shared';
 
 export const HousePayment = () => {
   return (
-    <section id={Style.house_section1}>
-      <div className={Style.house_container}>
-        <Image className={Style.singleHouseImage} src="" alt="" />
-        <div className={Style.small_image_container}>
+    <section id={Styles.house_section1}>
+      <div className={Styles.house_container}>
+        <Image className={Styles.singleHouseImage} src="" alt="" />
+        <div className={Styles.small_image_container}>
           <div>
-            <Image className={Style.small_image} src={house2} alt="" />
+            <Image className={Styles.small_image} src={house2} alt="" />
+            <div className={Styles.slide_left}>
+              <SlidebarButton />
+            </div>
           </div>
           <div>
-            <Image className={Style.small_image} src={house3} alt="" />
+            <Image className={Styles.small_image} src={house3} alt="" />
           </div>
           <div>
-            <Image className={Style.small_image} src={house4} alt="" />
+            <Image className={Styles.small_image} src={house4} alt="" />
+            <div>
+              <SlidebarButton />
+            </div>
           </div>
         </div>
       </div>
