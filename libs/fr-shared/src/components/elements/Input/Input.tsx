98c29b551fb9ad1b9d8d98fae06905a8128478dd
nooshinbarazi <nooshinbry@gmail.com>
2023-03-06 17:17:58 +0330

    fix: add placeholder and error to input component

--- libs/fr-shared/src/components/elements/Input/Input.tsx
+++ libs/fr-shared/src/components/elements/Input/Input.tsx
@@ -1,25 +1,33 @@
 import React from 'react';
-import { FieldValues, UseFormRegister } from 'react-hook-form';
+import styles from './Input.module.scss';
+import {RegisterOptions,UseFormRegisterReturn } from 'react-hook-form';
 
 interface IProps {
   Icon: React.ReactNode;
   name: string;
   type: string;
   id: string;
-  required: boolean;
-  register: UseFormRegister<FieldValues>;
+  validation: any;
+  placeholder: string;
+  error:any
+  register: (name: string, options?: RegisterOptions) => UseFormRegisterReturn;
 }
 
-export const Input = ({Icon, name, type, id, required, register,}: IProps) => {
+export const Input = ({Icon, name, type, id, validation, placeholder,error, register,}: IProps) => {
   return (
     <>
-      <div>{Icon}</div>
+    <div className={styles.input}>
+      <div className={styles.input_icon}>{Icon}</div>
       <input
-        {...register(name, {required})}
+        {...register(name, validation)}
         name={name}
         type={type}
         id={id}
+        placeholder={placeholder}
+        className={styles.input_text}
       />
+    </div>
+      <span className={styles.error_msg}>{error}</span>
     </>
   );
 };
