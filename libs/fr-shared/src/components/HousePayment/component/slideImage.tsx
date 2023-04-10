20ff81d9633fe6a21564be2064da1f14537d8c11
ranaweysi <ranaweysi@gmail.com>
2023-04-10 11:41:20 +0430

    feat: create slide image and modal for housePayment page

--- libs/fr-shared/src/components/HousePayment/component/slideImage.tsx
+++ libs/fr-shared/src/components/HousePayment/component/slideImage.tsx
@@ -1,18 +1,31 @@
 import React from 'react';
 import Image from 'next/image';
-import Styles from './slideImage.module.scss';
+import styles from './slideImage.module.scss';
 import { SliderPath } from '../../elements/HouseImageSlider/SliderPath';
 import { SlidebarButton } from '../../elements/Icons/SliderButton';
-import { useState } from 'react';
+import { Close} from '../../elements/Icons/Close';
+import { Slider } from '../../elements/Icons/Slider';
+import { Album } from '../../elements/Icons/Album';
+import { useState,useEffect } from 'react';
 
 interface IProps {
   images: any[];
+  houses:any[];
 }
 
-
-
-export const SlideImage = ({ images }: IProps) => {
+export const SlideImage = ({ images,houses }: IProps) => {
   const [currentImageIndex, setCurrentImagIndex] = useState(0);
+  const [isHovered, setIsHovered] = useState(false);
+  const [modal, setModal] = useState(false);
+  const [handelActive, setHandelActive] = useState(false);
+
+  const handeleClick = () => {
+    setHandelActive((handelActive) => !handelActive);
+  };
+
+  const toggleModal = () => {
+    setModal(!modal);
+  };
 
   const prevSlide = () => {
     const resetToVeryBack = currentImageIndex === 0;
@@ -26,36 +39,125 @@ export const SlideImage = ({ images }: IProps) => {
     setCurrentImagIndex(index);
   };
 
-  const ShowImage = () => {
-  }
+  useEffect(() => {
+    if (!isHovered) {
+      const intervalId = setInterval(() => {
+        nextSlide();
+      }, 6000);
+      return () => clearInterval(intervalId);
+    }
+  }, [isHovered, nextSlide]);
+
+  const currentImage = houses[currentImageIndex];
+
+  const prevImage =
+  currentImageIndex === 0
+    ? houses[houses.length - 1]
+    : houses[currentImageIndex - 1];
+
+const nextImage =
+  currentImageIndex === houses.length - 1
+    ? houses[0]
+    : houses[currentImageIndex + 1];
+
+
+  const ShowImage = () => {};
   return (
-    <section id={Styles.house_section1}>
-      <div className={Styles.house_container}>
-        <div className={Styles.slide_left}>
-          <button onClick={prevSlide}>
-            <SlidebarButton />
-          </button>
-        </div>
-        <div className={Styles.image_container}>
-          <div className={Styles.single_Image}>
+    <section id={styles.house_section1}>
+      <div className={styles.house_container}>
+        <div className={styles.image_container}>
+          <div className={styles.single_Image}>
             <Image src={images[currentImageIndex]} alt="" />
           </div>
-          <div className={Styles.small_image_container}>
-          {images.map((image,index) => {
+          <div className={styles.small_image_container} onClick={toggleModal}>
+            {images.map((image, index) => {
               return (
                 <div key={index}>
-                  <Image src={image} alt="" onClick={()=>setCurrentImagIndex(index)} className={currentImageIndex == index ? 'active' :''} />
+                  <Image
+                    src={image}
+                    alt="/"
+                    onClick={() => setCurrentImagIndex(index)}
+                    className={currentImageIndex == index ? 'active' : ''}
+                  />
                 </div>
               );
             })}
           </div>
         </div>
-        <div className={Styles.slide_right}>
-          <button onClick={nextSlide}>
-            <SlidebarButton />
-          </button>
+        <div className={styles.counter_container}>
+          <div className={styles.text_coounter}>
+            <button onClick={toggleModal}>+{images.length}</button>
+          </div>
         </div>
       </div>
+      {modal && (
+          <div
+            className={styles.containers}
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
+          >
+            <div className={styles.conatiner}>
+              <div className={styles.Header_container}>
+                <div className={styles.Header_left}>
+                  <button className={styles.close_button} onClick={toggleModal}>
+                    <Close />
+                  </button>
+                </div>
+                <div className={styles.Header_right}>
+                  <button onClick={handeleClick} className={handelActive ? styles.deactived_button  : styles.actived_button}>
+                    <Slider />
+                    <p className={styles.text_btn}>slider</p>
+                  </button>
+                  <button onClick={handeleClick} className={handelActive ? styles.actived_button : styles.deactived_button} >
+                    <Album/>
+                    <p className={styles.text_btn}>Album</p>
+                  </button>
+                </div>
+              </div>
+              <div className={styles.box}>
+                <SliderPath />
+                <button
+                  className={styles.slider_button_prev}
+                  onClick={prevSlide}
+                >
+                  <SlidebarButton />
+                </button>
+                <div className={styles.slider}>
+                  <div className={styles.prev_image}>
+                    <Image
+                      src={prevImage.images.img}
+                      alt=""
+                      fill
+                      object-fit="cover"
+                    />
+                  </div>
+                  <div className={styles.current_image}>
+                    <Image
+                      src={currentImage.images.img}
+                      alt=""
+                      fill
+                      object-fit="cover"
+                    />
+                  </div>
+                  <div className={styles.next_image}>
+                    <Image
+                      src={nextImage.images.img}
+                      alt=""
+                      fill
+                      object-fit="cover"
+                    />
+                  </div>
+                </div>
+                <button
+                  className={styles.slider_button_next}
+                  onClick={nextSlide}
+                >
+                  <SlidebarButton />
+                </button>
+              </div>
+            </div>
+          </div>
+        )}
     </section>
   );
 };
