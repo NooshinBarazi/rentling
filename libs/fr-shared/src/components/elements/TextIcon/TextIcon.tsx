8c418028807141c807d6b610fe492edb7c8d7b59
nooshinbarazi <nooshinbry@gmail.com>
2023-04-10 17:40:46 +0430

    fix: edit textIcon component

--- libs/fr-shared/src/components/elements/TextIcon/TextIcon.tsx
+++ libs/fr-shared/src/components/elements/TextIcon/TextIcon.tsx
@@ -3,14 +3,18 @@ import style from './texticon.module.scss';
 
 interface IProps {
   Icon: React.ReactNode;
+  title: string;
   text: string;
   newStyle: string;
 }
 
-export const TextIcon = ({ Icon, text, newStyle }: IProps) => {
+export const TextIcon = ({ Icon,title, text, newStyle }: IProps) => {
   return (
     <div className={newStyle || style.text_icon}>
+      <div className={style.text_header}>
       <span>{Icon}</span>
+      <h4>{title}</h4>
+      </div>
       <p>{text}</p>
     </div>
   );
