dded4d5810e3a9146a5e106f892d05f1827acbd6
nooshinbarazi <nooshinbry@gmail.com>
2023-04-21 14:40:30 +0430

    feat: create a template that is common to all profile pages

---
+++ libs/fr-shared/src/components/pages/Profile/components/profileTemplate/ProfileTemplate.tsx
@@ -0,0 +1,103 @@
+import styles from './ProfileTemplate.module.scss';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
+import {
+  CurrentDateIcon,
+  FavoritesIcon,
+  HouseIcon,
+  PaymentIcon,
+  SecurityIcon,
+  UserProfileIcon,
+} from '@rentling/fr-shared';
+import React from 'react';
+
+
+export const ProfileTemplate = ({children }: {children: React.ReactNode}) => {
+  const router = useRouter();
+  return (
+    <section className={styles.profile}>
+      <div className={styles.container}>
+        <div className={styles.profile_nav}>
+          <nav>
+            <Link href="/profile">
+              <li
+                className={
+                  router.pathname === '/profile'
+                    ? styles.nav_item_active
+                    : styles.nav_item
+                }
+              >
+                <UserProfileIcon />
+                <h4>Profile</h4>
+                
+              </li>
+            </Link>
+            <Link href="/profile/current/" as={`/profile/current/`}>
+              <li
+                className={
+                  router.pathname === '/profile/current/'
+                    ? styles.nav_item_active
+                    : styles.nav_item
+                }
+              >
+                <CurrentDateIcon />
+                <h4>current rental</h4>
+              </li>
+            </Link>
+            <Link href="/profile/fav">
+              <li
+                className={
+                  router.pathname === '/profile/fav'
+                    ? styles.nav_item_active
+                    : styles.nav_item
+                }
+              >
+                <FavoritesIcon />
+                <h4>Favorites</h4>
+              </li>
+            </Link>
+
+            <Link href="/profile/history">
+              <li
+                className={
+                  router.pathname === '/profile/history'
+                    ? styles.nav_item_active
+                    : styles.nav_item
+                }
+              >
+                <HouseIcon />
+                <h4>rental history</h4>
+              </li>
+            </Link>
+            <Link href="/profile/payment">
+              <li
+                className={
+                  router.pathname === '/profile/payment'
+                    ? styles.nav_item_active
+                    : styles.nav_item
+                }
+              >
+                <PaymentIcon />
+                <h4>payments</h4>
+              </li>
+            </Link>
+            <Link href="/profile/security">
+              <li
+                className={
+                  router.pathname === '/profile/security'
+                    ? styles.nav_item_active
+                    : styles.nav_item
+                }
+              >
+                <SecurityIcon />
+                <h4>Login & security</h4>
+              </li>
+            </Link>
+          </nav>
+        </div>
+        <div className={styles.profile_content}>{children}</div>
+      </div>
+    </section>
+  );
+};
