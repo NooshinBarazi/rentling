a97c4937cbbc11d513040952e63068a3e8dbe9a9
nooshinbarazi <nooshinbry@gmail.com>
2023-04-21 14:43:58 +0430

    feat: create layout for profile page and change app file for when the pages have layout

---
+++ apps/web-app/pages/profile/current/[id].tsx
@@ -0,0 +1,48 @@
+import { useRouter } from 'next/router';
+import { useState, useEffect } from 'react';
+import { CurrentRental, EmptyCurrentRental, users } from '@rentling/fr-shared';
+import { ProfileLayout } from '../../layout/ProfileLayout';
+
+export default function CurrentRentalPage() {
+  const router = useRouter();
+  const { id } = router.query;
+  const [user, setUser] = useState(null);
+
+  
+  useEffect(() => {
+      if (id) {
+          const user = getCurrentuserById(id as string);
+          setUser(user);
+        }
+    }, [id]);
+    
+    const getCurrentuserById = (userId: string) => {
+      return users.find((user) => user.profile.id === userId);
+    };
+    
+  if (!user) {
+    return <EmptyCurrentRental />;
+  }
+  return (
+    <CurrentRental
+      id={user.currentHouse.id}
+      title={user.currentHouse.title}
+      img={user.currentHouse.images.img}
+      region={user.currentHouse.feature.region}
+      city={user.currentHouse.feature.city}
+      address={user.currentHouse.address}
+      price={user.currentHouse.price.thirtyDay}
+      duration={user.currentHouse.duration}
+      startDate={user.currentHouse.rentalPeriod.startDate}
+      endDate={user.currentHouse.rentalPeriod.endDate}
+      bedrooms={user.currentHouse.feature.bedroom}
+      bathrooms={user.currentHouse.feature.bathroom}
+      parking={user.currentHouse.feature.parking}
+      area={user.currentHouse.feature.meterage}
+      description={user.currentHouse.description}
+      comment={user.currentHouse.comment}
+    />
+  );
+};
+
+ CurrentRentalPage.PageLayout = ProfileLayout;
