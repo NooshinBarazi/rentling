a2e90503eeeeecda942c25252c7ed7d3d3d1b62c
nooshinbarazi <nooshinbry@gmail.com>
2023-04-24 23:01:34 +0430

    feat: export new component

--- libs/fr-shared/src/components/index.ts
+++ libs/fr-shared/src/components/index.ts
@@ -63,5 +63,6 @@ export * from './elements/Icons/DropDownIcon';
 export * from './elements/Icons/FindHomeIcon';
 export * from './pages/Profile/components/StarRating/StarRating';
 export * from './pages/Profile/components/profileTemplate/ProfileTemplate';
+export * from './pages/Profile/components/common/MoreInfo/MoreInfo';
 
 
