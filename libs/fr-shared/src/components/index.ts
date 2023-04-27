172a837d19cb5f50523a0ed0f20bd00c4ac5b7a2
nooshinbarazi <nooshinbry@gmail.com>
2023-04-28 00:29:26 +0430

    feat: export component

--- libs/fr-shared/src/components/index.ts
+++ libs/fr-shared/src/components/index.ts
@@ -72,7 +72,9 @@ export * from './elements/Icons/PaymentIcon';
 export * from './elements/Icons/SecurityIcon';
 export * from './elements/Icons/DropDownIcon';
 export * from './elements/Icons/FindHomeIcon';
+export * from './elements/Icons/LessInfoIcon';
 export * from './pages/Profile/components/StarRating/StarRating';
 export * from './pages/Profile/components/profileTemplate/ProfileTemplate';
 export * from './pages/Profile/components/common/MoreInfo/MoreInfo';
+export * from './pages/Profile/components/data';
 
