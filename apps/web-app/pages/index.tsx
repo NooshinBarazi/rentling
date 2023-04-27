16d2817142143fec25fc3240267d5b4ee6a885fd
nooshinbarazi <nooshinbry@gmail.com>
2023-04-28 00:33:27 +0430

    fix: leave the component empty

--- apps/web-app/pages/index.tsx
+++ apps/web-app/pages/index.tsx
@@ -1,6 +1,5 @@
-import { RentalHistory } from '@rentling/fr-shared';
 import styles from './index.module.scss';
-import { HousePayment, Landing } from '@rentling/fr-shared';
+import {HousePayment,Landing } from '@rentling/fr-shared';
 
 export function Index() {
   /*
@@ -9,16 +8,12 @@ export function Index() {
    * Note: The corresponding styles are in the ./index.scss file.
    */
 
-<<<<<<< HEAD
-  return <div><RentalHistory /></div>;
-=======
   return (
     <div>
       <Landing />
       {/* <HousePayment /> */}
     </div>
   );
->>>>>>> beadadb38781eda9dedc11b19d98bb656829445d
 }
 
 export default Index;
