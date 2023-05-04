b240b0cc555d251167942ce3db27ce9a4582ba40
nooshinbarazi <nooshinbry@gmail.com>
2023-05-04 13:15:48 +0430

    feat: add security page

---
+++ apps/web-app/pages/profile/security/[id].tsx
@@ -0,0 +1,10 @@
+import { Security } from '@rentling/fr-shared';
+import { ProfileLayout } from '../../layout/ProfileLayout';
+
+export default function SecurityPage() {
+  return (
+   <Security />
+  );
+}
+
+SecurityPage.PageLayout = ProfileLayout;
