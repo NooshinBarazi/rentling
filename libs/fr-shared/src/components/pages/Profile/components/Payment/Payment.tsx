701aff886a8a2e8b8f9e783d07a755d4bc736823
ranaweysi <ranaweysi@gmail.com>
2023-05-07 09:38:58 +0430

    feat: added dropdown in payment page

--- libs/fr-shared/src/components/pages/Profile/components/Payment/Payment.tsx
+++ libs/fr-shared/src/components/pages/Profile/components/Payment/Payment.tsx
@@ -5,28 +5,60 @@ import { useState } from 'react';
 export const Payment = () => {
   const [display, setDisplay] = useState(false);
 
+  const toggle_show = () => {
+    setDisplay(!display)
+  }
+
   return (
     <>
-      <div className={styles.container}>
+      <div className={display? styles.container_show : styles.container}>
         <div className={styles.header_container}>
           <div className={styles.header_favorite_houses}>
             <p>your favorite houses</p>
           </div>
         </div>
         <div className={styles.container_boxs}>
-          <div className={styles.box_top}>
-            <div className={styles.description}>
-              <p>Your payments</p>
-              <span>Keep track of all your payments and refunds.</span>
+          <div className={styles.container_box_top}>
+            <div className={styles.box_top}>
+              <div className={styles.description}>
+                <p>Your payments</p>
+                <span>Keep track of all your payments and refunds.</span>
+              </div>
+              <div className={display? styles.less_detaile : styles.more_detaile}>
+                <button
+                  onClick={toggle_show}
+                >
+                  {display? 'See less details' : 'See more details' } <span></span>
+                </button>
+              </div>
             </div>
-            <div className={styles.btn}>
-              <button
-                onClick={() => {
-                  setDisplay(true);
-                }}
-              >
-                See more details <span></span>
-              </button>
+            <div className={display? styles.container_show_detaile : styles.no_show}>
+              <div className={styles.description}>
+                <p>payments summary :</p>
+              </div>
+              <div className={styles.total_price}>
+                <span> 1,900 $ </span>
+                <p>Total spent money for rented houses</p>
+              </div>
+              <div className={styles.description}>
+                <p>payments history :</p>
+              </div>
+              <div className={styles.price_date_container}>
+                <div className={styles.price}>
+                  <span>1,110 $ </span>
+                </div>
+                <div className={styles.input_date}>
+                  <input type="date" placeholder="start Date" required />
+                </div>
+              </div>
+              <div className={styles.price_date_container}>
+                <div className={styles.price}>
+                  <span>800 $ </span>
+                </div>
+                <div className={styles.input_date}>
+                  <input type="date" placeholder="start Date" required />
+                </div>
+              </div>
             </div>
           </div>
           <div className={styles.box_bottom}>
