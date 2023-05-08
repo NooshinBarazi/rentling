0aec56dfd74200e056dbc334a596f2a8b9c0e2ca
ranaweysi <ranaweysi@gmail.com>
2023-05-08 10:58:01 +0430

    feat: create paymentMethod card

--- libs/fr-shared/src/components/pages/Profile/components/PaymentPages/PaymentMethod/PaymnetMethod.tsx
+++ libs/fr-shared/src/components/pages/Profile/components/PaymentPages/PaymentMethod/PaymnetMethod.tsx
@@ -1,30 +1,86 @@
 import Image from 'next/image';
 import styles from './paymnetMethod.module.scss';
-import image1 from './vector/Vector1.jpg';
-import image2 from './vector/Vector2.jpg';
-import image3 from './vector/Vector3.jpg';
-import image4 from './vector/Vector4.jpg';
+import image from './vector/Frame 416.jpg';
+import { Button, SelectInput } from '@rentling/fr-shared';
+import { useForm } from 'react-hook-form';
 
-export const PaymentMethod = () => {
+interface IOption {
+  value: string;
+  label: string;
+}
+
+interface PropsPaymentMethod {
+  options: IOption[];
+}
+
+export const PaymentMethod = (
+  onSubmit: any,
+  { options }: PropsPaymentMethod
+) => {
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm();
+
   return (
     <section className={styles.main}>
       <div className={styles.container}>
         <div className={styles.card_container}>
           <div className={styles.hedear}>
-            <div  className={styles.image1}>
-              <Image src={image1} alt="/" fill object-fit="cover" />
-            </div>
-            <div className={styles.image2}>
-              <Image src={image2} alt="/" fill object-fit="cover" />
-            </div>
-            <div className={styles.image3}>
-              <Image src={image3} alt="/" fill object-fit="cover" />
-            </div>
-            <div className={styles.image4}>
-              <Image src={image4} alt="/" fill object-fit="cover" />
+            <div className={styles.image_container}>
+              <Image src={image} alt="/" fill object-fit="cover" />
             </div>
           </div>
-          <form></form>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className={styles.card_number_input}>
+              <input
+                defaultValue={'Card Number'}
+                {...register('exampleRequired', { required: true })}
+              />
+              {errors.exampleRequired && <span>This field is required</span>}
+            </div>
+            <div className={styles.card_information}>
+              <input
+                defaultValue={'Expiration'}
+                {...register('exampleRequired', { required: true })}
+              />
+              {errors.exampleRequired && <span>This field is required</span>}
+              <input
+                defaultValue={'CVV'}
+                {...register('exampleRequired', { required: true })}
+              />
+              {errors.exampleRequired && <span>This field is required</span>}
+            </div>
+            <div className={styles.select_option}>
+              <SelectInput
+                defaultValue={'Country/Region'}
+                name={'Country/Region'}
+                register={register}
+                options={[
+                  { value: 'United States', label: 'United States' },
+                  { value: 'United States', label: 'United States' },
+                  { value: 'United States', label: 'United States' },
+                  { value: 'United States', label: 'United States' },
+                  { value: 'United States', label: 'United States' },
+                ]}
+              />
+            </div>
+            <div className={styles.button_handel_submit}>
+              <Button
+                Icon={undefined}
+                text="CANCEL"
+                newStyle=""
+                onClick={() => {}}
+              />
+              <Button
+                Icon={undefined}
+                text="DONE"
+                newStyle=""
+                onClick={() => {}}
+              />
+            </div>
+          </form>
         </div>
       </div>
     </section>
