c3e704fe6b4d479af72b31cdf39ffc28316ea1ce
nooshinbarazi <nooshinbry@gmail.com>
2023-05-08 14:32:17 +0430

    feat: create sharemodal component to copy the url of house

--- libs/fr-shared/src/components/elements/HouseCard/ShareModal.tsx
+++ libs/fr-shared/src/components/elements/HouseCard/ShareModal.tsx
@@ -1,44 +1,27 @@
-import { ShareIcon } from '@rentling/fr-shared';
+import { CheckIcon, ShareIcon } from '@rentling/fr-shared';
 import { useState } from 'react';
-import styles from './ShareModal.module.scss'
 
 interface shareProps {
   url: string;
 }
 export const ShareModal = ({ url }: shareProps) => {
-  const [showModal, setShowModal] = useState(false);
+  const [isShare, setIsShare] = useState(false);
 
-  const handleShare = (service: string) => {
-    switch (service) {
-      case 'facebook':
-        window.open(`https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(url)}`,'_blank');
-        break;
-      case 'linkedin':
-        window.open(`https://www.linkedin.com/shareArticle?url=${encodeURIComponent(url)}`,'_blank');
-        break;
-        case 'email':
-            window.location.href = `mailto:?body=${encodeURIComponent(url)}`;
-            break;
-          case 'copy':
-            navigator.clipboard.writeText(url);
-            break;
-            default:
-            break;
-    }
-    setShowModal(false);
+  const handleShare = () => {
+    navigator.clipboard.writeText(url);
+    setIsShare(true);
+    setTimeout(() => {
+      setIsShare(false);
+    }, 2000);
   };
 
   return (
     <div>
-      <div onClick={() => setShowModal(!showModal)}>
-        <ShareIcon />
-      </div>
-      {showModal && (
-        <div className={styles.share_modal}>
-          <button onClick={()=> handleShare('facebook')}>Share on Facebook</button>
-          <button onClick={()=> handleShare('linkedin')}>Share on LinkedIn</button>
-          <button onClick={()=> handleShare('email')}>Share via Email</button>
-          <button onClick={()=> handleShare('copy')}>Copy Link</button>
+      {isShare ? (
+        <CheckIcon />
+      ) : (
+        <div onClick={handleShare}>
+          <ShareIcon />
         </div>
       )}
     </div>
