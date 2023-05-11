af3c5efef121297fa9415b588ce69d080410aadf
nooshinbarazi <nooshinbry@gmail.com>
2023-05-11 13:39:10 +0430

    feat: Provide the Redux Store to React

--- apps/web-app/pages/index.tsx
+++ apps/web-app/pages/index.tsx
@@ -1,12 +1,14 @@
+import { Provider } from 'react-redux';
 import styles from './index.module.scss';
 import { Landing } from '@rentling/fr-shared';
+import {store} from '../../../libs/fr-shared/src/store/store'
 
 export function Index() {
 
   return (
-    <>
+    <Provider store={store}>
       <Landing />
-    </>
+    </Provider>
   );
 }
 
