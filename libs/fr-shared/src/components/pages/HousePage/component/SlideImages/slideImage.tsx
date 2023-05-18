fcfe26ca9ed47d6c2eb30f1e2f6a6991bbcb4e22
Rana Weysi <ranaweysi@gmail.com>
2023-05-18 23:42:57 +0330

    feat: create responsive for slideImage component

--- libs/fr-shared/src/components/pages/HousePage/component/SlideImages/slideImage.tsx
+++ libs/fr-shared/src/components/pages/HousePage/component/SlideImages/slideImage.tsx
@@ -8,6 +8,10 @@ interface IProps {
   images: any[];
 }
 
+export const ModalSlider = () => {
+  return <></>;
+};
+
 export const SlideImage = ({ images }: IProps) => {
   const [currentImageIndex, setCurrentImagIndex] = useState(0);
   const [modalSlider, setModalSlider] = useState(false);
@@ -61,7 +65,8 @@ export const SlideImage = ({ images }: IProps) => {
       : images[currentImageIndex + 1];
 
   return (
-    <section className={styles.house_section1}>
+    <>
+      {/* <section className={styles.house_section1}> */}
       <div className={styles.house_container}>
         <div className={styles.image_container}>
           <div className={styles.single_image}>
@@ -105,143 +110,145 @@ export const SlideImage = ({ images }: IProps) => {
           </div>
         </div>
       </div>
+      {/* </section> */}
+
       {modalSlider && (
         <div className={styles.container_modal_slider}>
-          <div className={styles.container_slider}>
-            <div className={styles.header_container_slider}>
-              <div className={styles.header_slider}>
-                <div className={styles.header_left}>
+          {/* <div className={styles.container_slider}> */}
+          <div className={styles.header_container_slider}>
+            <div className={styles.header_slider}>
+              <div className={styles.header_left}>
+                <button
+                  className={styles.close_button}
+                  onClick={toggleModalSLider}
+                >
+                  <Close />
+                  <p className={styles.text_btn_close}>close</p>
+                </button>
+              </div>
+              <div className={styles.header_right}>
+                <div className={styles.button_slider}>
                   <button
-                    className={styles.close_button}
-                    onClick={toggleModalSLider}
+                    onClick={() => setHandelActive(true)}
+                    className={
+                      handelActive
+                        ? styles.actived_button
+                        : styles.deactived_button
+                    }
                   >
-                    <Close />
-                    <p className={styles.text_btn_close}>close</p>
+                    <Slider />
+                    <p className={styles.text_btn}>slider</p>
                   </button>
                 </div>
-                <div className={styles.header_right}>
-                  <div className={styles.button_slider}>
-                    <button
-                      onClick={() => setHandelActive(true)}
-                      className={
-                        handelActive
-                          ? styles.actived_button
-                          : styles.deactived_button
-                      }
-                    >
-                      <Slider />
-                      <p className={styles.text_btn}>slider</p>
-                    </button>
-                  </div>
-                  <div className={styles.button_album}>
-                    <button
-                      onClick={(event) => {
-                        setHandelActive(false);
-                        toggleModalAlbum();
-                        toggleModalSLider();
-                      }}
-                      className={
-                        handelActive
-                          ? styles.deactived_button
-                          : styles.actived_button
-                      }
-                    >
-                      <Album />
-                      <p className={styles.text_btn}>Album</p>
-                    </button>
-                  </div>
+                <div className={styles.button_album}>
+                  <button
+                    onClick={(event) => {
+                      setHandelActive(false);
+                      toggleModalAlbum();
+                      toggleModalSLider();
+                    }}
+                    className={
+                      handelActive
+                        ? styles.deactived_button
+                        : styles.actived_button
+                    }
+                  >
+                    <Album />
+                    <p className={styles.text_btn}>Album</p>
+                  </button>
                 </div>
               </div>
             </div>
-            <div className={styles.box}>
-              <button className={styles.slider_button_prev} onClick={prevSlide}>
-                <SlidebarButton />
-              </button>
-              <div className={styles.slider}>
-                <div className={styles.prev_image}>
-                  <Image src={prevImage} alt="" fill object-fit="cover" />
+          </div>
+          <div className={styles.box}>
+            <button className={styles.slider_button_prev} onClick={prevSlide}>
+              <SlidebarButton />
+            </button>
+            <div className={styles.slider}>
+              <div className={styles.prev_image}>
+                <Image src={prevImage} alt="" fill object-fit="cover" />
+              </div>
+              <div className={styles.current_image_slider}>
+                <Image src={currentImage} alt="" fill object-fit="cover" />
+              </div>
+              <div className={styles.next_image}>
+                <Image src={nextImage} alt="" fill object-fit="cover" />
+              </div>
+            </div>
+            <button className={styles.slider_button_next} onClick={nextSlide}>
+              <SlidebarButton />
+            </button>
+          </div>
+        </div>
+        // </div>
+      )}
+
+      {modalAlbum && (
+        <div className={styles.container_album}>
+          <div className={styles.container_header_album}>
+            <div className={styles.header_album}>
+              <div className={styles.header_left}>
+                <button
+                  className={styles.close_button}
+                  onClick={(event) => {
+                    toggleModalAlbum();
+                  }}
+                >
+                  <Close />
+                  <p className={styles.text_btn_close}>close</p>
+                </button>
+              </div>
+              <div className={styles.header_right}>
+                <div className={styles.button_slider}>
+                  <button
+                    className={
+                      handelActive
+                        ? styles.actived_button
+                        : styles.deactived_button
+                    }
+                    onClick={(event) => {
+                      setHandelActive(true);
+                      toggleModalAlbum();
+                      toggleModalSLider();
+                    }}
+                  >
+                    <Slider />
+                    <p className={styles.text_btn}>slider</p>
+                  </button>
                 </div>
-                <div className={styles.current_image_slider}>
-                  <Image src={currentImage} alt="" fill object-fit="cover" />
-                </div>
-                <div className={styles.next_image}>
-                  <Image src={nextImage} alt="" fill object-fit="cover" />
+                <div className={styles.button_album}>
+                  <button
+                    className={
+                      handelActive
+                        ? styles.deactived_button
+                        : styles.actived_button
+                    }
+                    onClick={() => {
+                      setHandelActive(false);
+                    }}
+                  >
+                    <Album />
+                    <p className={styles.text_btn}>Album</p>
+                  </button>
                 </div>
               </div>
-              <button className={styles.slider_button_next} onClick={nextSlide}>
-                <SlidebarButton />
-              </button>
             </div>
+          </div>
+          <div className={styles.current_image_album}>
+            {images.map((img, i) => {
+              return (
+                <div
+                  className={styles.grid_images}
+                  key={i}
+                  onClick={togglePopup}
+                >
+                  <img src={img} alt="/" onClick={() => onSelectImage(i)} />
+                </div>
+              );
+            })}
           </div>
         </div>
       )}
-
-        {modalAlbum && (
-      <div className={styles.container_album}>
-            <div className={styles.container_header_album}>
-              <div className={styles.header_album}>
-                <div className={styles.header_left}>
-                  <button
-                    className={styles.close_button}
-                    onClick={(event) => {
-                      toggleModalAlbum();
-                    }}
-                  >
-                    <Close />
-                    <p className={styles.text_btn_close}>close</p>
-                  </button>
-                </div>
-                <div className={styles.header_right}>
-                  <div className={styles.button_slider}>
-                    <button
-                      className={
-                        handelActive
-                          ? styles.actived_button
-                          : styles.deactived_button
-                      }
-                      onClick={(event) => {
-                        setHandelActive(true);
-                        toggleModalAlbum();
-                        toggleModalSLider();
-                      }}
-                    >
-                      <Slider />
-                      <p className={styles.text_btn}>slider</p>
-                    </button>
-                  </div>
-                  <div className={styles.button_album}>
-                    <button
-                      className={
-                        handelActive
-                          ? styles.deactived_button
-                          : styles.actived_button
-                      }
-                      onClick={() => {
-                        setHandelActive(false);
-                      }}
-                    >
-                      <Album />
-                      <p className={styles.text_btn}>Album</p>
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className={styles.current_image_album}>
-              {images.map((img, i) => {
-                return (
-                  <div
-                    className={styles.grid_images}
-                    key={i}
-                    onClick={togglePopup}
-                  >
-                    <img src={img} alt="/" onClick={() => onSelectImage(i)} />
-                  </div>
-                );
-              })}
-            </div>
-      </div>
-        )}
 
       {popup && (
         <div className={styles.container_popup}>
@@ -262,6 +269,6 @@ export const SlideImage = ({ images }: IProps) => {
           </div>
         </div>
       )}
-    </section>
+    </>
   );
 };
