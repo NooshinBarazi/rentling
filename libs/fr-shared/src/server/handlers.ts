d78c559d88c694353f8d03733d03af4fade68355
NaderBhrr <nader.bhr.devv@gmail.com>
2023-05-24 11:19:28 +0330

    feat(Mocking API): Add the mock APIs for rentalHistory and currentRental

--- libs/fr-shared/src/server/handlers.ts
+++ libs/fr-shared/src/server/handlers.ts
@@ -33,6 +33,31 @@ export const handlers = [
     const favoritesForUser = users.find(
       (u) => u.profile.id === userId
     )?.favoritesList;
+
     return res(ctx.status(200), ctx.json(favoritesForUser));
   }),
+
+  rest.get(`${MOCK_API_URL}/:userId/rental-history`, (req, res, ctx) => {
+
+    const { userId } = req.params;
+
+    const rentalHistoryForUser = users.find(
+      (u) => u.profile.id === userId
+    )?.history;
+
+    return res(ctx.status(200), ctx.json(rentalHistoryForUser));
+
+  }),
+
+  rest.get(`${MOCK_API_URL}/:userId/current-rental`, (req, res, ctx) => {
+
+    const { userId } = req.params;
+
+    const currentRentalForUser = users.find(
+      (u) => u.profile.id === userId
+    )?.currentHouse;
+
+    return res(ctx.status(200), ctx.json(currentRentalForUser));
+
+  })
 ];
