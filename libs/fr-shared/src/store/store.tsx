65b4dcaeab905e08bc4df6175e13babb7cf99d9d
nooshinbarazi <nooshinbry@gmail.com>
2023-05-25 19:02:26 +0430

    feat: add createWrapper to store

--- libs/fr-shared/src/store/store.tsx
+++ libs/fr-shared/src/store/store.tsx
@@ -1,6 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { createWrapper} from "next-redux-wrapper";
 import FavoriteReducer from './features/favoriteSlice';
 import authReducer from './features/authSlice';
+
+const makeStore = ()=>{
+  const store = configureStore({
+    reducer: {
+      favorites: FavoriteReducer,
+      auth: authReducer,
+    },
+  });
+  return store
+}
 
 export const store = configureStore({
   reducer: {
@@ -9,5 +20,10 @@ export const store = configureStore({
   },
 });
 
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
+export type AppStore = ReturnType<typeof makeStore>;
+export type RootState = ReturnType<AppStore['getState']>;
+export type AppDispatch = AppStore['dispatch'];
+
+
+export const wrapper = createWrapper<AppStore>(makeStore);
+
