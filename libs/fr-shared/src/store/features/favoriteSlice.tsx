f7c4575654776aa86914961d040d3d0cd0df8704
nooshinbarazi <nooshinbry@gmail.com>
2023-05-25 19:08:46 +0430

    feat: fetch data from api with createAsyncThunk

--- libs/fr-shared/src/store/features/favoriteSlice.tsx
+++ libs/fr-shared/src/store/features/favoriteSlice.tsx
@@ -1,26 +1,53 @@
-import { PayloadAction, createSlice } from '@reduxjs/toolkit';
+import { PayloadAction, createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { HYDRATE } from 'next-redux-wrapper';
 
 export type Favorite = {
   id: string;
+  img: string;
+  region: string;
+  title: string;
+  dailyPrice: string;
+  monthlyPrice: string;
 };
 
-type FavoriteState = Favorite[];
-const initialState: FavoriteState = [];
+type FavoriteState = Favorite[]
+const initialState: FavoriteState = []
+
+export const fetchFavorite = createAsyncThunk(
+  'favorites',
+  async (userId, { rejectWithValue }) => {
+    try {
+      const response = await window.fetch(
+        `http://localhost:8080/${userId}/favorites`
+      );
+      const responseData  = await response.json();
+      console.log(responseData )
+      return responseData ;
+    } catch (error) {
+      return rejectWithValue(error);
+    }
+  }
+);
 
 const favoritesSlice = createSlice({
   name: 'favorites',
   initialState,
   reducers: {
-    addFavorite: (state, { payload: houseId }: PayloadAction<string>) => {
-      const foundHouse = state.find((element) => element.id === houseId);
+    addFavorite: (state, { payload: house }: PayloadAction<Favorite>) => {
+      const foundHouse = state.find((element) => element.id === house.id);
       if (foundHouse) {
-        const index = state.findIndex((element) => element.id === houseId);
+        const index = state.findIndex((element) => element.id === house.id);
         state.splice(index, 1);
       } else {
-        state.push({ id: houseId });
+        state.push(house);
       }
     },
   },
+  extraReducers:{
+    [HYDRATE]: (state, action)=>{
+    return action.payload.favorites
+    }
+  }
 });
 
 export const { addFavorite } = favoritesSlice.actions;
